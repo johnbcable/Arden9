c945ffb1f4bc8c35ea927073accc082aa626a828
John Cable <johnbcable@me.com>
2019-06-22 20:32:38 +0100

    iMac commit to mirro that on MacBook

--- js/9mileraceresults.js
+++ js/9mileraceresults.js
@@ -163,7 +163,7 @@ $(document).ready(function() {
 	getCategoryList();	// Pull in all the categories agains the year they were used
 	
 	// Restrict filtered categories to those used in intYear
-	intYear = 2018;
+	intYear = 2019;
 	filteredCategories = categories.filter((obj) => obj.RaceYear === intYear);
 	
 	jsonstring = JSON.stringify(filteredCategories);
